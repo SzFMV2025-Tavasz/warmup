aead05aa511497a8c2be83beb50b4c231f71d94e
Roland Kertész <roland.kertesz@itsquad.hu>
2025-03-04 21:30:54 +0100

    fix(DummySensor): fix distance not refreshing on dashboard

--- src/AutomatedCar/SystemComponents/DummySensor.cs
+++ src/AutomatedCar/SystemComponents/DummySensor.cs
@@ -9,11 +9,13 @@
 
     public class DummySensor : SystemComponent
     {
-        private VirtualFunctionBus vfb { get; set; }
         private AutomatedCar automatedCar { get; set; }
+
+        private DummyPacket dummyPacket { get; set; }
         public DummySensor(VirtualFunctionBus virtualFunctionBus, AutomatedCar automatedCar) : base(virtualFunctionBus)
         {
-            this.vfb = virtualFunctionBus;
+            this.dummyPacket = new DummyPacket();
+            virtualFunctionBus.DummyPacket = dummyPacket;
             this.automatedCar = automatedCar;
         }
 
@@ -22,15 +24,11 @@
             var circles = World.Instance.WorldObjects.FindAll(x => (x as Circle) != null);
             Circle circle = circles.FirstOrDefault() as Circle;
 
-
             int distanceXDiff = Math.Abs(circle.X - automatedCar.X);
             int distanceYDiff = Math.Abs(circle.Y - automatedCar.Y);
 
-            vfb.DummyPacket = new DummyPacket()
-            {
-                DistanceX = distanceXDiff,
-                DistanceY = distanceYDiff
-            };
+            dummyPacket.DistanceX = distanceXDiff;
+            dummyPacket.DistanceY = distanceYDiff;
         }
     }
 }
