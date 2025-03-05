402986f94f77ba1068a7834a4a7cf6db0462b9ea
Dániel Varga <daniel.varga@stud.uni-obuda.hu>
2025-03-05 21:07:42 +0100

    Add DummySensor class

---
+++ src/AutomatedCar/SystemComponents/DummySensor.cs
@@ -0,0 +1,22 @@
+﻿namespace AutomatedCar.SystemComponents
+{
+    using AutomatedCar.SystemComponents.Packets;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class DummySensor : SystemComponent
+    {
+        public DummySensor(VirtualFunctionBus virtualFunctionBus) : base(virtualFunctionBus)
+        {
+        }
+        private VirtualFunctionBus virtualFunctionBus;
+
+        public override void Process()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
