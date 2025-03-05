98a16eda07a4e206664023c0e6963fd9d5a692e3
mparajdi@stud.uni-obuda.hu <mparajdi@stud.uni-obuda.hu>
2025-03-05 15:57:40 +0100

    DummySensor created

---
+++ src/AutomatedCar/SystemComponents/Sensors/DummySensor.cs
@@ -0,0 +1,25 @@
+﻿namespace AutomatedCar.SystemComponents.Sensors
+{
+    using AutomatedCar.Models;
+    using AutomatedCar.SystemComponents.Packets;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class DummySensor : SystemComponent
+    {
+        public DummySensor(VirtualFunctionBus virtualFunctionBus) : base(virtualFunctionBus)
+        {
+        }
+        DummyPacket dummyPacket;
+        Circle circle = World.Instance.WorldObjects.FirstOrDefault(wo => wo is Circle) as Circle;
+        AutomatedCar autoCar = World.Instance.ControlledCar;
+        public override void Process()
+        {
+            dummyPacket.DistanceX = autoCar.X - circle.X;
+            dummyPacket.DistanceY = autoCar.Y - circle.Y;
+        }
+    }
+}
