93cc52bb23bba90a953b445bdb6979a0c78d3436
Babics Flóra <babicsflora@gmail.com>
2025-03-05 23:33:43 +0100

    DummySensor implemented

---
+++ src/AutomatedCar/SystemComponents/DummySensor.cs
@@ -0,0 +1,28 @@
+﻿using AutomatedCar.SystemComponents;
+using AutomatedCar.SystemComponents.Packets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedCar.Models
+{
+    public class DummySensor : SystemComponent
+    {
+        private DummyPacket dummyPacket;
+        public DummySensor(VirtualFunctionBus virtualFunctionBus) : base(virtualFunctionBus)
+        {
+            this.dummyPacket = new DummyPacket();
+            virtualFunctionBus.DummyPacket = dummyPacket;
+        }
+
+        public override void Process()
+        {
+            var car = World.Instance.WorldObjects.First(worldObject => worldObject.GetType().Name == "AutomatedCar");
+            var circle = World.Instance.WorldObjects.First(worldObject => worldObject.GetType().Name == "Circle");
+            this.dummyPacket.DistanceX = car.X - circle.X;
+            this.dummyPacket.DistanceY = car.Y - circle.Y;
+        }
+    }
+}
